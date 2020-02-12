b5dd2378aae7ca532af7936b1d0eeff6787bd7b2
Konstantin Lekomtsev <lekomtsev.konst@gmail.com>
2020-02-12 12:11:44 -0800

    refactoring analysis

--- setup.py
+++ setup.py
@@ -24,5 +24,10 @@ setup(
     author='Konstantin Lekomtsev',
     license='MIT',
     packages=['titanic'],
-	install_requires=['pypandoc>=1.4']
+	install_requires=['pypandoc>=1.4',
+					  'pytest>=4.3.1',
+					  'pytest-runner>=4.4',
+					  ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest']
 )
